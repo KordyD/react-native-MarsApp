bc587a82f83706a73d102f90a10ec44e1a0b672a
KordyD <84672403+KordyD@users.noreply.github.com>
2023-08-05 14:28:06 +0300

    Initial commit, add fonts and splash

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
+import { useFonts } from 'expo-font';
+import { Dosis_400Regular, Dosis_600SemiBold } from '@expo-google-fonts/dosis';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useState } from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Dosis_400Regular, Dosis_600SemiBold });
+
+  const [language, setLanguage] = useState('');
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.header}>Select Camera and Date</Text>
+      <StatusBar style='auto' />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#DCCEBE',
+  },
+  header: {
+    fontFamily: 'Dosis_600SemiBold',
+    fontSize: 18,
+    alignSelf: 'center',
   },
 });
