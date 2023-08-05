4b1f758adb61a2fccd4f97a8ef05e5c357583bd3
KordyD <84672403+KordyD@users.noreply.github.com>
2023-08-05 21:42:52 +0300

    Add basic components

--- App.js
+++ App.js
@@ -4,13 +4,30 @@ import { StatusBar } from 'expo-status-bar';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback, useState } from 'react';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { Button, NativeBaseProvider, Select, FormControl } from 'native-base';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Dosis_400Regular, Dosis_600SemiBold });
 
-  const [language, setLanguage] = useState('');
+  const cameras = [
+    { abbr: 'FHAZ', label: 'Front Hazard Avoidance Camera' },
+    { abbr: 'RHAZ', label: 'Rear Hazard Avoidance Camera' },
+    { abbr: 'MAST', label: 'Mast Camera' },
+    { abbr: 'CHEMCAM', label: 'Chemistry and Camera Complex' },
+    { abbr: 'MAHLI', label: 'Mars Hand Lens Imager' },
+    { abbr: 'MARDI', label: 'Mars Descent Imager' },
+    { abbr: 'NAVCAM', label: 'Navigation Camera' },
+    { abbr: 'PANCAM', label: 'Panoramic Camera' },
+    {
+      abbr: 'MINITES',
+      label: 'Miniature Thermal Emission Spectrometer (Mini-TES)',
+    },
+  ];
+
+  const URL = '';
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -18,15 +35,57 @@ export default function App() {
     }
   }, [fontsLoaded]);
 
+  const [camera, setCamera] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [error, setError] = useState('');
+
   if (!fontsLoaded) {
     return null;
   }
 
+  const handleSubmit = () => {
+    try {
+      setError('');
+      if (!camera) {
+        throw new Error('Please, select a camera');
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
-    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={styles.header}>Select Camera and Date</Text>
-      <StatusBar style='auto' />
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <Text style={styles.header}>Select Camera and Date</Text>
+        <FormControl isInvalid={error}>
+          <Select
+            selectedValue={camera}
+            placeholder='Choose camera'
+            onValueChange={(value) => setCamera(value)}
+          >
+            {cameras.map((item) => (
+              <Select.Item
+                key={item.abbr}
+                label={item.label}
+                value={item.abbr}
+              />
+            ))}
+          </Select>
+          <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
+          <DateTimePicker
+            value={date}
+            mode='date'
+            maximumDate={new Date()}
+            onChange={(event, date) => setDate(date)}
+          />
+          <Button variant='solid' onPress={handleSubmit}>
+            Explore
+          </Button>
+        </FormControl>
+        <StatusBar style='auto' />
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
 
